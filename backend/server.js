d855e4c7146e72c8b8358ffb0ed1c7932af5917b
Milad <miladakbari.dev@gmail.com>
2022-05-02 10:00:40 +0430

    Added all

---
+++ backend/server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const path = require('path')
+
+const Todo = require('./Models/Todo')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+dotenv.config()
+
+mongoose
+  .connect(process.env.ATLAS_URI)
+  .then(() => console.log('Connected to DB'))
+  .catch((err) => console.log(err))
+
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find()
+  res.json(todos)
+})
+
+app.post('/todo/new', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  })
+
+  todo.save()
+  res.json(todo)
+})
+
+app.delete('/todo/delete/:id', async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id)
+  res.json(result)
+})
+
+app.put('/todo/complete/:id', async (req, res) => {
+  const todo = await Todo.findById(req.params.id)
+  todo.completed = !todo.completed
+  todo.save()
+  res.json(todo)
+})
+
+// ------------------ Deployment ----------------------
+
+__dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send('API running')
+  })
+}
+// ------------------ Deployment ----------------------
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`)
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,145 @@
+import { useEffect, useState } from 'react'
+function App() {
+  const [todos, setTodos] = useState([])
+  const [popupActive, setPopupActive] = useState(false)
+  const [newTodo, setNewTodo] = useState('')
+  const getTodos = () => {
+    fetch(`${process.env.REACT_APP_API_URL}/todos`)
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((err) => console.error(err))
+  }
+
+  const completeTodo = async (id) => {
+    const data = await fetch(
+      `${process.env.REACT_APP_API_URL}/todo/complete/${id}`,
+      {
+        method: 'PUT',
+      }
+    ).then((res) => res.json())
+
+    setTodos((todos) =>
+      todos.map((todo) => {
+        if (todo._id === data._id) {
+          todo.completed = data.completed
+        }
+        return todo
+      })
+    )
+  }
+
+  const deleteTodo = async (id) => {
+    const data = await fetch(
+      `${process.env.REACT_APP_API_URL}/todo/delete/${id}`,
+      {
+        method: 'DELETE',
+      }
+    ).then((res) => res.json())
+    setTodos((todos) => todos.filter((todo) => todo._id !== data._id))
+  }
+
+  const addTodo = async () => {
+    const data = await fetch(`${process.env.REACT_APP_API_URL}/todo/new`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text: newTodo,
+      }),
+    }).then((res) => res.json())
+
+    setTodos([...todos, data])
+    setPopupActive(false)
+    setNewTodo('')
+  }
+
+  useEffect(() => {
+    getTodos()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Welcome Milad</h1>
+      <h4>Your tasks</h4>
+
+      {todos.length === 0 && (
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <div style={{ position: 'absolute', top: '50%' }}>
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: '10px',
+                fontSize: '30px',
+              }}
+            >
+              😐{' '}
+            </div>
+            <div style={{ fontSize: '20px' }}>You have no tasks</div>
+          </div>
+        </div>
+      )}
+      <div className="todos">
+        {todos.map((todo) => (
+          <div
+            className={'todo ' + (todo.completed ? 'is-complete' : '')}
+            key={todo._id}
+          >
+            <div className="checkbox"></div>
+            <div className="text">{todo.text}</div>
+
+            <div
+              className={
+                !todo.completed ? 'complete-todo' : 'undo-complete-todo'
+              }
+              onClick={() => completeTodo(todo._id)}
+            >
+              {!todo.completed ? <span>&#10004;</span> : <span>&#8211;</span>}
+            </div>
+            <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
+              x
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className="addPopup" onClick={() => setPopupActive(true)}>
+        +
+      </div>
+
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            x
+          </div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input
+              type="text"
+              className="add-todo-input"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            <div className="button" onClick={addTodo}>
+              Create Task
+            </div>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  )
+}
+
+export default App
